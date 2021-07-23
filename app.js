4baa1b6feaed460bcacc7c82c97cf7e2fcbb949a
GreyDaCaLa <gdele9164@gmail.com>
2021-07-22 21:20:21 -0400

    My Initial Commit -- Fixes

--- app.js
+++ app.js
@@ -6,14 +6,81 @@ console.log(
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
 
+let a =20;
+let b = 4;
+
+let add =  a+b;
+console.log(`add: ${add}`)
+let minus = a-b;
+console.log(`minus: ${minus}`)
+let multiply = a*b;
+console.log(`multiply: ${multiply}`)
+let dividing = a/b;
+console.log(`dividing: ${dividing}`)
+
+
 // YOUR CODE HERE
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
+
+a = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
+
+
+console.log(`- What is the value of: a + str?`)
+console.log(a+str)
+console.log(`- What is the value of: a + str2?`)
+console.log(a+str2)
+console.log(`- What is the value of: a + isPresent?`)
+console.log(a+isPresent)
+console.log(`- What is the value of: a + firstName?`)
+console.log(a+firstName)
+console.log(`- What is the value of: a + lastName?`)
+console.log(a+lastName)
 
 // YOUR CODE HERE
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
+
+a = 5;
+str = "5";
+str2 = "five";
+isPresent = false;
+firstName = "Robin";
+lastName = "Williams";
+
+
+console.log(`- What is the value of: a == str?`)
+console.log(a == str)
+console.log(`- What is the value of: a === str?`)
+console.log(a === str)
+console.log(`- What is the value of: !isPresent?`)
+console.log(!isPresent)
+console.log(`- What is the value of: (“eleven” == str2 && a >= str)?`)
+console.log(('eleven' == str2 && a >= str))
+console.log(`- What is the value of: (!isPresent || isPresent)?`)
+console.log((!isPresent || isPresent))
+console.log(`- What is the value of: 0 == false?`)
+console.log( 0 == false)
+console.log(`- What is the value of: 0 === false?`)
+console.log(0 === false)
+console.log(`- What is the value of: 0 != false?`)
+console.log(0 != false)
+console.log(`- What is the value of 0 !== false?`)
+console.log(0 !== false)
+
+
+
 // YOUR CODE HERE
+
+
+
+
+
